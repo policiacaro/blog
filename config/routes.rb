35afa16b7f47159e83439ecc4697bc5d4d79f8b8
David Yarhi <yarhi.david@gmail.com>
2022-02-18 16:18:07 +0900

    changed articles to lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles, except: :index
+  root to: 'lists#index'
+  resources :lists, except: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
